e496006c6e7f9c3e40ac35068428167d9338df8b
ELITENICK <nikita_shimenkov2011@mail.ru>
2024-12-10 00:13:04 +0300

    Add files via upload

---
+++ oculus_killer.bat
@@ -0,0 +1,35 @@
+REM Killall oculus to get rid of the botnets... or if Oculus Link just is stubborn for some reason...
+REM Killing everything Oculus related, and then opening the Oculus app again usually fixes it
+REM Run as admin!
+
+REM Multiples because theyre stubborn sometimes
+
+
+taskkill /f /im OculusClient.exe
+taskkill /f /im OVRRedir.exe
+taskkill /f /im oculus-platform-runtime.exe
+taskkill /f /im OVRServer_x64.exe
+taskkill /f /im OVRServiceLauncher.exe
+taskkill /f /im OculusClient.exe
+taskkill /f /im OVRRedir.exe
+taskkill /f /im oculus-platform-runtime.exe
+taskkill /f /im OVRServer_x64.exe
+taskkill /f /im OVRServiceLauncher.exe
+taskkill /f /im OculusClient.exe
+taskkill /f /im OVRRedir.exe
+taskkill /f /im oculus-platform-runtime.exe
+taskkill /f /im OVRServer_x64.exe
+taskkill /f /im OVRServiceLauncher.exe
+taskkill /f /im OculusClient.exe
+taskkill /f /im OVRRedir.exe
+taskkill /f /im oculus-platform-runtime.exe
+taskkill /f /im OVRServer_x64.exe
+taskkill /f /im OVRServiceLauncher.exe
+taskkill /f /im OculusClient.exe
+taskkill /f /im OVRRedir.exe
+taskkill /f /im oculus-platform-runtime.exe
+taskkill /f /im OVRServer_x64.exe
+taskkill /f /im OVRServiceLauncher.exe
+
+
+pause
